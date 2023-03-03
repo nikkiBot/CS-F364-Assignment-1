6bf0a1dfd1c6e07318eaa98bc1139ce08ca9fe76
Anshul-Kanodia <f20200174@hyderabad.bits-pilani.ac.in>
2023-03-03 22:34:28 +0530

    fixed seg fault, DCEl implemented, more testing needed

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std ;
 class Edge;
-
+class Face;
 
 class Vertex {
     public:
@@ -20,12 +20,12 @@ class Edge {
     public:
         Vertex *origin = NULL;
         Edge *twin = NULL;
-        //Face *left = NULL;
+        Face *left = NULL;
         Edge *next = NULL;
         Edge *prev = NULL;
 };
 
-Edge* addEdge(Vertex *start1, Vertex *end1) {
+Edge* addEdge(Vertex *start1, Vertex *end1, Face* face_id_int, Face* face_id_ext) {
     Edge* e = new Edge();
     Edge *f = new Edge();
     
@@ -36,29 +36,63 @@ Edge* addEdge(Vertex *start1, Vertex *end1) {
     e->twin->prev = e->next ;
     e->twin->next = e->prev ;
     start1->inc_edge = e;
+    e->left = face_id_int;
+    e->twin->left = face_id_ext;
     return e;
 }
 
+class Face {
+    public:
+        Edge *incident_edge = NULL; 
+        int id = 0;
+};
+
+class DCEL {
+    public:
+        //vector<Vertex> vertices;
+        vector<Edge*> edges;
+        //vector<Face> faces;
+
+        void makeDCEL(vector<Vertex*> v, int interior, int exterior) {
+            if(v.size()<2) {
+                return;
+            }
+            Face *f_int = new Face();
+            Face *f_ext = new Face();
+            f_int->id = interior;
+            f_ext->id = exterior;
+            Edge* e = addEdge(v[0], v[1], f_int, f_ext);
+            f_ext->incident_edge = e->twin;
+            f_int->incident_edge = e;
+            edges.push_back(e);
+            for(int i = 1; i<v.size()-1; i++) {
+                Edge* e2 = addEdge(v[1],v[2],f_int,f_ext);
+                edges.push_back(e2);
+            }
+            Edge* closing = addEdge(v[v.size()-1],v[0],f_int, f_ext);
+            edges.push_back(closing);
+        }
+
+        void PrintDCEL() {
+            
+            for(int i = 0; i<edges.size(); i++) {
+                cout<<"Edge "<<(i+1)<<" Starts  from ("<<edges[i]->origin->coordinates.first<<","<<edges[i]->origin->coordinates.second<<")\n";
+                cout<<"Edge "<<(i+1)<<" ends  from ("<<edges[i]->twin->origin->coordinates.first<<","<<edges[i]->twin->origin->coordinates.second<<")\n";
+            }
+        }
+};
+
 int main() {
-    Vertex *v1 = new Vertex(1,1);
-    //cout<<v->coordinates.first<<" "<<v->coordinates.second<<endl;
-    Vertex *v2 = new Vertex(2,2);
-    Vertex *v3 = new Vertex(3,3);
-    Edge *e1 = new Edge();
-    Edge *e2 = new Edge();
-    e1=addEdge(v1,v2);
-    e2=addEdge(v2,v3);
-    
-    Vertex* temp = v1;
-    //print edges for debugging:
-    while(temp!=NULL) {
-        cout << temp->coordinates.first << " " << temp->coordinates.second << endl;
-        if(temp->inc_edge->twin->origin) {
-            temp = temp->inc_edge->twin->origin;
-        }
-        else {
-            break;
-        }
-    }
+    vector<Vertex*> v;
+    Vertex* v1 = new Vertex(1,1);
+    Vertex* v2 = new Vertex(2,2);
+    Vertex* v3 = new Vertex(3,3);
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+
+    DCEL* d  = new DCEL();
+    d->makeDCEL(v, 0,1);
+    d->PrintDCEL();
     return 0;
 }
